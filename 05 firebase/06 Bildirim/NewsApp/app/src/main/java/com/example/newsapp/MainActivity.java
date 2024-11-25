dfe82eff669320e286a80835c8dd6198e013db4f
Ekrem Dermanel <ekremdermanel@gmail.com>
2024-11-25 23:44:09 +0300

    update

---
+++ 05 firebase/06 Bildirim/NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.newsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+
+        FirebaseMessaging.getInstance().getToken()
+                        .addOnCompleteListener(new OnCompleteListener<String>() {
+                            @Override
+                            public void onComplete(@NonNull Task<String> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.e("FirebaseError", "Fetching FCM registration token failed", task.getException());
+                                    return;
+                                }
+                                String token = task.getResult();
+                                editText.setText(token);
+                                Log.i("FirebaseInfo", "Token : " + token);
+                            }
+                        });
+
+
+    }
+    
+    public void copy(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("token", editText.getText().toString());
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Copied!", Toast.LENGTH_SHORT).show();
+    }
+}
